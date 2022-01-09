53bfeb9c2d2e71cd1327358640d15996be86102c
Shamal Jayathilake <54285211+shamaljayathilake@users.noreply.github.com>
2022-01-09 13:13:25 +0530

    db updated

---
+++ db/db_assert_registry_old.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2021 at 10:32 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_assert_registry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assets`
+--
+
+CREATE TABLE `assets` (
+  `asset_id` int(11) NOT NULL,
+  `asset_name` varchar(255) NOT NULL,
+  `asset_owner` varchar(100) NOT NULL,
+  `asset_description` text NOT NULL,
+  `asset_location` varchar(55) NOT NULL,
+  `criticality` enum('Critical','Medium','Low') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assets`
+--
+
+INSERT INTO `assets` (`asset_id`, `asset_name`, `asset_owner`, `asset_description`, `asset_location`, `criticality`) VALUES
+(1, 'Cisco Router', 'Nimal Karunasekara', 'Cisco router with serial ID 123483924 has been issued for the on prem server hosted by IAM project', 'RD-SER-123', 'Critical');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `password` text NOT NULL,
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `reset_token` text DEFAULT NULL,
+  `access_level` enum('Admin','Editor','Viewer') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` ( `user_id`,`name`,`email`,`emp_id`, `password`, `added_on`, `reset_token`,`access_level`) VALUES
+(1,'Admin','admin@ar.com',1,'$2b$12$08UvYJ23BIjSFgCQHaW8dOH6tH0Lfa57eCy0gWNjGrJeMsMeZPR8C', '2021-11-19 09:31:09', NULL, 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assets`
+--
+ALTER TABLE `assets`
+  ADD PRIMARY KEY (`asset_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `unique_email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assets`
+--
+ALTER TABLE `assets`
+  MODIFY `asset_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
